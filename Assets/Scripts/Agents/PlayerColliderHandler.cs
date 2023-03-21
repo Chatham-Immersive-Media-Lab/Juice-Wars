68c0b1b2c356c511d67f1e0b6da1f253003a94eb
hunterdyar <bloop@hdyar.com>
2023-03-21 12:21:49 -0400

    Player can get hurt, I suppose

---
+++ Assets/Scripts/Agents/PlayerColliderHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Agents
+{
+	//This script only exists because I want the player script on the parent object and the collider on a child :p
+	public class PlayerColliderHandler : MonoBehaviour
+	{
+
+		private Player _player;
+
+		private void Awake()
+		{
+			_player = GetComponentInParent<Player>();
+		}
+
+		private void OnCollisionEnter2D(Collision2D col)
+		{
+			var bullet = col.gameObject.GetComponent<Bullet>();
+			if (bullet != null)
+			{
+				_player.TakeDamage(bullet.damage,col.GetContact(0).point);
+			}
+		}
+	}
+}
